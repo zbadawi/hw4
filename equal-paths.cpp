e24e90fc37c351135926e856cc7968af7757fbe8
Zena Badawi <zbadawi@usc.edu>
2023-06-19 13:01:07 +0000

    equal paths

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -1,13 +1,32 @@
 #include "equal-paths.h"
+#include <algorithm>
 using namespace std;
 
 
 // You may add any prototypes of helper functions here
+// Helper function to calculate the height of a tree
+int calculateHeight(Node* node) {
+    if (node == nullptr){
+        return 0;
+    }
 
+    int leftHeight = calculateHeight(node->left);
+    int rightHeight = calculateHeight(node->right);
+    return 1 + max(leftHeight, rightHeight);
+}
 
 bool equalPaths(Node * root)
 {
-    // Add your code below
+    if (root == nullptr){
+        return true;
+    }
 
+    int leftHeight = calculateHeight(root->left);
+    int rightHeight = calculateHeight(root->right);
+
+    if (leftHeight != rightHeight){
+        return false;
+    }
+
+    return equalPaths(root->left) && equalPaths(root->right);
 }
-
